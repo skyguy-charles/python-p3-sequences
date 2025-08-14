17fba4cc073f051ad28aab620fa394081060d214
charles <cyphersmae@gmail.com>
2025-08-14 23:56:42 +0300

    sequences p3

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
-#!/usr/bin/env python3
-
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    if n <= 0:
+        print([])
+        return
+    elif n == 1:
+        print([0])
+        return
+    elif n == 2:
+        print([0, 1])
+        return
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < n:
+        next_value = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_value)
+    
+    print(fib_sequence)
+if __name__ == "__main__":
+    print_fibonacci(9)
